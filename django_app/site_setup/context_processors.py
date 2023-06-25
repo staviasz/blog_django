52c3fc927a1f84e6194fc8cfd545ffafdde70047
staviasz <erickesc21@hotmail.com>
2023-06-25 00:06:25 -0300

    relacionamento do site_setup com o MenuLinks e criação do context_processors para injeção de dados em todas as templates

---
+++ django_app/site_setup/context_processors.py
@@ -0,0 +1,10 @@
+from site_setup.models import SiteSetup
+
+def site_setup(request):
+  data = SiteSetup.objects.order_by('-id').first()
+
+  return {
+    'site_setup': {
+      'title': data,
+    }
+  }
