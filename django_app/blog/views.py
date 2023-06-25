8eb2c63d8630bc106030739718733d2b203fe23e
staviasz <erickesc21@hotmail.com>
2023-06-25 19:34:33 -0300

    configuração dos templates

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -6,3 +6,16 @@ def index(request):
     request,
     'blog/pages/index.html',
   )
+
+
+def post(request):
+  return render(
+    request,
+    'blog/pages/post.html',
+  )
+
+def page(request):
+  return render(
+    request,
+    'blog/pages/page.html',
+  )
