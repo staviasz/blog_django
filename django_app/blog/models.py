13432cd0e5eea2be0c2410c233b086ba0ed53bcd
staviasz <erickesc21@hotmail.com>
2023-06-26 12:42:24 -0300

    desenvolvimento do model Tag

--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from utils.rands import slugify_new
 
+class Tag(models.Model):
+  class Meta:
+    verbose_name = "Tag"
+    verbose_name_plural = "Tags"
 
+  name = models.CharField(max_length=255)
+  slug = models.SlugField(
+    unique=True,
+    default=None,
+    null=True,
+    blank=True,
+    max_length=100
+  )
+
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify_new(self.name)
+    return super().save(*args, **kwargs)
