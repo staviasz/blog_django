d1daf6cf40ce6a22c4081ade34469c59b56f0410
staviasz <erickesc21@hotmail.com>
2023-06-26 13:00:31 -0300

    desenvolvimento do model Category e Page

--- django_app/blog/admin.py
+++ django_app/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag
+from blog.models import Tag, Category, Page
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -11,3 +11,30 @@ class TagAdmin(admin.ModelAdmin):
   prepopulated_fields = {
     'slug': ('name',),
   }
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display  = 'id', 'name', 'slug',
+  list_display_links = 'name',
+  search_fields = 'id', 'name', 'slug',
+  list_per_page = 10
+  ordering = '-id',
+  prepopulated_fields = {
+    'slug': ('name',),
+  }
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+  list_display  = 'id', 'title', 'is_published',
+  list_display_links = 'title',
+  search_fields = 'id', 'slug', 'title', 'content',
+  list_per_page = 50
+  list_filter = 'is_published',
+  list_editable = 'is_published',
+  ordering = '-id',
+  prepopulated_fields = {
+    'slug': ('title',),
+  }
+
--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -19,3 +19,48 @@ class Tag(models.Model):
     if not self.slug:
       self.slug = slugify_new(self.name)
     return super().save(*args, **kwargs)
+
+  def __str__(self):
+    return self.name
+
+
+class Category(models.Model):
+  class Meta:
+    verbose_name = "Category"
+    verbose_name_plural = "Categories"
+
+  name = models.CharField(max_length=255)
+  slug = models.SlugField(
+    unique=True,
+    default=None,
+    null=True,
+    blank=True,
+    max_length=100
+  )
+
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify_new(self.name)
+    return super().save(*args, **kwargs)
+
+  def __str__(self):
+    return self.name
+
+
+class Page(models.Model):
+  title = models.CharField(max_length=255)
+  slug = models.SlugField(
+    unique=True,
+    default=None,
+    null=True,
+    blank=True,
+    max_length=100
+  )
+  is_published = models.BooleanField(
+    default=False,
+    help_text='this field needs to be checked for the post to be displayed'
+  )
+  content = models.TextField()
+
+  def __str__(self):
+    return self.title
