9c0699c3032216f9858c658851935ab94bdaa0bc
staviasz <erickesc21@hotmail.com>
2023-06-26 15:35:47 -0300

    desenvolvimento do model Post com redimensionamento de imagens

--- django_app/blog/admin.py
+++ django_app/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -38,3 +38,24 @@ class PageAdmin(admin.ModelAdmin):
     'slug': ('title',),
   }
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display  = 'id', 'title', 'is_published', 'created_by',
+  list_display_links = 'title',
+  search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+  list_per_page = 50
+  list_filter = 'category', 'is_published',
+  list_editable = 'is_published',
+  ordering = '-id',
+  readonly_fields = 'created_at', 'updated_at', 'created_by', 'updated_by',
+  prepopulated_fields = {
+    'slug': ('title',),
+  }
+
+  def save_model(self, request, obj, form, change):
+    if change:
+      obj.updated_by = request.user
+    else:
+      obj.created_by = request.user
+
+    obj.save()
--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from utils.rands import slugify_new
+from django.contrib.auth.models import User
+from utils.resize_image import resize_image
 
 class Tag(models.Model):
   class Meta:
@@ -61,6 +63,89 @@ class Page(models.Model):
     help_text='this field needs to be checked for the post to be displayed'
   )
   content = models.TextField()
+
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify_new(self.name)
+    return super().save(*args, **kwargs)
+
+  def __str__(self):
+    return self.title
+
+
+class Post(models.Model):
+  class Meta:
+    verbose_name = "Post"
+    verbose_name_plural = "Posts"
+
+  title = models.CharField(max_length=255)
+  slug = models.SlugField(
+    unique=True,
+    default=None,
+    null=True,
+    blank=True,
+    max_length=100
+  )
+  excerpt = models.CharField(max_length=255)
+  is_published = models.BooleanField(
+      default=False,
+      help_text='this field needs to be checked for the post to be displayed'
+    )
+  content = models.TextField()
+  cover = models.ImageField(
+    upload_to='post/%Y/%m/',
+    blank=True,
+    default=None
+  )
+  cover_in_post_content = models.BooleanField(
+    default=True,
+    help_text='Display the cover image inside the post too'
+  )
+  created_at = models.DateTimeField(auto_now_add=True)
+  created_by = models.ForeignKey(
+    User,
+    on_delete=models.SET_NULL,
+    blank=True,
+    null=True,
+    # serve para buscar o valor dentro do template ex: user.post_created_by.all
+    related_name='post_created_by'
+  )
+  updated_at = models.DateTimeField(auto_now=True)
+  updated_by = models.ForeignKey(
+    User,
+    on_delete=models.SET_NULL,
+    blank=True,
+    null=True,
+    related_name='post_updated_by'
+  )
+  category = models.ForeignKey(
+    Category,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    default=None
+  )
+  tags = models.ManyToManyField(
+    Tag,
+    blank=True,
+    default=''
+    )
+
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify_new(self.name)
+
+    current_cover_name = str(self.cover.name)
+    super_save = super().save(*args, **kwargs)
+    cover_changed = False
+
+    if self.cover:
+      cover_changed = current_cover_name != self.cover.name
+
+    if cover_changed:
+      resize_image(self.cover, 900)
+
+    return super_save
 
   def __str__(self):
     return self.title
